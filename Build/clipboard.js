76d50cb7129e3d78caa36ed1b326f713e711a05f
Jihunkim95 <darak551@naver.com>
2024-07-22 11:31:25 +0900

    feat : 동적 해상도 WebGL, component 추가

--- Build/clipboard.js
+++ Build/clipboard.js
@@ -1,21 +1,21 @@
-function CopyToClipboard(text) {
-    var str = UTF8ToString(text);
-    navigator.clipboard.writeText(str).then(function() {
-        console.log('Text copied to clipboard');
-    }).catch(function(error) {
-        console.error('Failed to copy text: ', error);
-    });
-}
+// function CopyToClipboard(text) {
+//     var str = UTF8ToString(text);
+//     navigator.clipboard.writeText(str).then(function() {
+//         console.log('Text copied to clipboard');
+//     }).catch(function(error) {
+//         console.error('Failed to copy text: ', error);
+//     });
+// }
 
-function GetClipboardText() {
-    navigator.clipboard.readText().then(function(text) {
-        console.log("Clipboard text:", text);
-        if (typeof Module.unityInstance !== 'undefined' && Module.unityInstance !== null) {
-            Module.unityInstance.SendMessage("TextCopyHandler", "SetInputFieldText", text);
-        } else {
-            console.error('unityInstance is not defined or null');
-        }
-    }).catch(function(error) {
-        console.error('Failed to read text: ', error);
-    });
-}
+// function GetClipboardText() {
+//     navigator.clipboard.readText().then(function(text) {
+//         console.log("Clipboard text:", text);
+//         if (typeof Module.unityInstance !== 'undefined' && Module.unityInstance !== null) {
+//             Module.unityInstance.SendMessage("TextCopyHandler", "SetInputFieldText", text);
+//         } else {
+//             console.error('unityInstance is not defined or null');
+//         }
+//     }).catch(function(error) {
+//         console.error('Failed to read text: ', error);
+//     });
+// }
